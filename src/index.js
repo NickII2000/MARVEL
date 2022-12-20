60830cbe2940efa52b3ea7fe31f4e8371f28ad99
Nick <Nick_II2000@mail.ru>
2022-12-20 19:46:57 +0600

    10-th commit (tast)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/App';
+import './index.css';
+import App from './App';
 
-import './style/style.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App/>,
   document.getElementById('root')
 );
+
+
