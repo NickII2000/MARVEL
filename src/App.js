3c83214b9f57824bb9ef60fecb52b49fdcb8cba8
Nick <Nick_II2000@mail.ru>
2022-12-23 19:40:55 +0600

    30-th commit

--- src/App.js
+++ src/App.js
@@ -51,28 +51,28 @@ import './App.css';
 
 const Slider = (props) => {
 
-    // // const slideStateArray = useState();
-    // // console.log(slideStateArray);
-    // const [slide, setSlide] = useState(0);
-    // const [autoplay, setAutoplay] = useState(false);
-
-    // function changeSlide(i) {
-    //     setSlide(siude => slide + i);
-    // }
-
-    // function toggleAutoplay() {
-    //     setAutoplay(autoplay => !autoplay);
-    // }
-
-    const [state, setState] = useState({ slide: 0, autoplay: false });
+    // const slideStateArray = useState();
+    // console.log(slideStateArray);
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
 
     function changeSlide(i) {
-        setState(state => ({ ...state, slide: state.slide + i }));
+        setSlide(siude => slide + i);
     }
 
     function toggleAutoplay() {
-        setState(state => ({ ...state, autoplay: !state.autoplay }));
+        setAutoplay(autoplay => !autoplay);
     }
+
+    // const [state, setState] = useState({ slide: 0, autoplay: false });
+
+    // function changeSlide(i) {
+    //     setState(state => ({ ...state, slide: state.slide + i }));
+    // }
+
+    // function toggleAutoplay() {
+    //     setState(state => ({ ...state, autoplay: !state.autoplay }));
+    // }
 
     return (
         <Container>
