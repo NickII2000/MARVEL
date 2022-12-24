3491fe65b286c966f8021fdb0eb06001b2a94ad5
Nick <Nick_II2000@mail.ru>
2022-12-25 01:29:33 +0600

    41-th commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
 
     rndCounter = () => {
         this.setState({
-            counter: +(Math.random() * (50 - -50) + -50).toFixed(0)
+            counter: +(Math.random() * (100 - -100) + -100).toFixed(0)
         })
     }
 
