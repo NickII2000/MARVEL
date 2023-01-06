7864f7f42d05bbb728a06ad498d921c51d50e7a6
Nick <Nick_II2000@mail.ru>
2023-01-06 20:06:26 +0600

    85-th commit

--- src/App.js
+++ src/App.js
@@ -3,105 +3,107 @@ import { Container } from 'react-bootstrap';
 import './App.css';
 
 
-// class Slider extends Component {
+class Slider extends Component {
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            autoplay: false,
+            slide: 0
+        }
+    }
 
-//     changeSlide = (i) => {
-//         this.setState(({ slide }) => ({
-//             slide: slide + i
-//         }))
-//     }
 
-//     toggleAutoplay = () => {
-//         this.setState(({ autoplay }) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
 
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br /> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
+    changeSlide = (i) => {
+        this.setState(({ slide }) => ({
+            slide: slide + i
+        }))
+    }
+
+    toggleAutoplay = () => {
+        this.setState(({ autoplay }) => ({
+            autoplay: !autoplay
+        }))
+    }
+
+    render() {
+        return (
+            <Container>
+                <div className="slider w-50 m-auto">
+                    <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                    <div className="text-center mt-5">Active slide {this.state.slide} <br /> {this.state.autoplay ? 'auto' : null}</div>
+                    <div className="buttons mt-3">
+                        <button
+                            className="btn btn-primary me-2"
+                            onClick={() => this.changeSlide(-1)}>-1</button>
+                        <button
+                            className="btn btn-primary me-2"
+                            onClick={() => this.changeSlide(1)}>+1</button>
+                        <button
+                            className="btn btn-primary me-2"
+                            onClick={this.toggleAutoplay}>toggle autoplay</button>
+                    </div>
+                </div>
+            </Container>
+        )
+    }
+}
+
+// const calcValue = () => {
+//     console.log('random');
+
+//     return Math.random() * (50 - 1) + 1;
 // }
 
-const calcValue = () => {
-    console.log('random');
+// const Slider = (props) => {
 
-    return Math.random() * (50 - 1) + 1;
-}
+//     // const slideStateArray = useState();
+//     // console.log(slideStateArray);
 
-const Slider = (props) => {
+//     const [slide, setSlide] = useState(() => calcValue());
+//     const [autoplay, setAutoplay] = useState(false);
 
-    // const slideStateArray = useState();
-    // console.log(slideStateArray);
+//     function changeSlide(i) {
+//         setSlide(siude => slide + i);
+//     }
 
-    const [slide, setSlide] = useState(() => calcValue());
-    const [autoplay, setAutoplay] = useState(false);
+//     function toggleAutoplay() {
+//         setAutoplay(autoplay => !autoplay);
+//     }
 
-    function changeSlide(i) {
-        setSlide(siude => slide + i);
-    }
+//     // const [state, setState] = useState({ slide: 0, autoplay: false });
 
-    function toggleAutoplay() {
-        setAutoplay(autoplay => !autoplay);
-    }
+//     // function changeSlide(i) {
+//     //     setState(state => ({ ...state, slide: state.slide + i }));
+//     // }
 
-    // const [state, setState] = useState({ slide: 0, autoplay: false });
+//     // function toggleAutoplay() {
+//     //     setState(state => ({ ...state, autoplay: !state.autoplay }));
+//     // }
 
-    // function changeSlide(i) {
-    //     setState(state => ({ ...state, slide: state.slide + i }));
-    // }
-
-    // function toggleAutoplay() {
-    //     setState(state => ({ ...state, autoplay: !state.autoplay }));
-    // }
-
-    return (
-        <Container>
-            <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br />
-                    {autoplay ? 'auto' : null}
-                </div>
-                <div className="buttons mt-3">
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
-                </div >
-            </div >
-        </Container >
-    )
-}
+//     return (
+//         <Container>
+//             <div className="slider w-50 m-auto">
+//                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+//                 <div className="text-center mt-5">Active slide {slide} <br />
+//                     {autoplay ? 'auto' : null}
+//                 </div>
+//                 <div className="buttons mt-3">
+//                     <button
+//                         className="btn btn-primary me-2"
+//                         onClick={() => changeSlide(-1)}>-1</button>
+//                     <button
+//                         className="btn btn-primary me-2"
+//                         onClick={() => changeSlide(1)}>+1</button>
+//                     <button
+//                         className="btn btn-primary me-2"
+//                         onClick={toggleAutoplay}>toggle autoplay</button>
+//                 </div >
+//             </div >
+//         </Container >
+//     )
+// }
 
 
 function App() {
