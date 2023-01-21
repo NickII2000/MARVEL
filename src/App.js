1353ef82bf14c9f43c76291eb8c8840d5ca70d52
Nick <Nick_II2000@mail.ru>
2023-01-21 19:29:55 +0600

    141-th commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { Component, useState, useEffect, useCallBack } from 'react';
+import { Component, useState, useEffect, useCallback } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-const getSomeImages = () => {
-    console.log('fetching');
-    return [
-        "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
-        "https://www.planetware.com/photos-large/USNY/usa-best-places-miami.jpg",
-        "https://www.planetware.com/wpimages/2022/09/best-luxury-all-inclusive-resorts-world-conrad-maldives-rangali-island-the-maldives.jpg"
-    ];
-}
+// const getSomeImages = () => {
+//     console.log('fetching');
+//     return [
+//         "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+//         "https://www.planetware.com/photos-large/USNY/usa-best-places-miami.jpg",
+//         "https://www.planetware.com/wpimages/2022/09/best-luxury-all-inclusive-resorts-world-conrad-maldives-rangali-island-the-maldives.jpg"
+//     ];
+// }
 
 const Slider = (props) => {
 
     const [slide, setSlide] = useState(0);
     const [autoplay, setAutoplay] = useState(false);
+
+    const getSomeImages = useCallback(() => {
+        console.log('fetching');
+        return [
+            "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+            "https://www.planetware.com/photos-large/USNY/usa-best-places-miami.jpg",
+            "https://www.planetware.com/wpimages/2022/09/best-luxury-all-inclusive-resorts-world-conrad-maldives-rangali-island-the-maldives.jpg"
+        ];
+    }, [slide]);
 
     function logging() {
         console.log('log!');
