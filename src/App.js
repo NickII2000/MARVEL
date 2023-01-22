1d76a2bd0c14a2904a4862dfb2e362be8f1a4499
Nick <Nick_II2000@mail.ru>
2023-01-22 18:25:21 +0600

    151-th commit

--- src/App.js
+++ src/App.js
@@ -72,11 +72,11 @@ const Slider = (props) => {
                 <div className="buttons mt-3">
                     <button
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-111)}>-1
+                        onClick={() => changeSlide(-1)}>-1
                     </button>
                     <button
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(111)}>+1
+                        onClick={() => changeSlide(1)}>+1
                     </button>
                     <button
                         className="btn btn-primary me-2"
