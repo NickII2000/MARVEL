ad99f25ce1ef41e9b5df8b9fc23aab7f2c5bd2de
Nick <Nick_II2000@mail.ru>
2023-04-03 23:28:04 +0600

    324-th commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const Form = () => {
     const [text, setText] = useState('');
 
-    const myRef = useRef(123 = 12);
+    const myRef = useRef(123 = 123);
 
     useEffect(() => {
         myRef.current = text;
