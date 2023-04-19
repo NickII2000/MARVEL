b7e8450a15e893c49c875a120183a4e20b46b244
Nick <Nick_II2000@mail.ru>
2023-04-19 07:24:00 +0600

    384-th commit

--- src/App.js
+++ src/App.js
@@ -30,14 +30,14 @@ const Slider = (props) => {
     }
 
     useEffect(() => {
-        // console.log('effect');
+        console.log('effect');
         document.title = `Slide: ${slide}`;
 
-        // window.addEventListener('click', logging);
+        window.addEventListener('click', logging);
 
-        // return () => {
-        //     window.removeEventListener('click', logging);
-        // }
+        return () => {
+            window.removeEventListener('click', logging);
+        }
 
     }, [slide]);
 
