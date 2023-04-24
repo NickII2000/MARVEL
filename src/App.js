d249477fe5d99c95c83976df9dbf3bdc54c9a552
Nick <Nick_II2000@mail.ru>
2023-04-24 07:38:58 +0600

    413-th commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const Form = () => {
 
     useEffect(() => {
         // myRef.current = text;
-        // myRef.current++;
+        myRef.current++;
         console.log(`text: ${text}`);
         console.log(`myRef.current: ${myRef.current}`);
     });
@@ -30,7 +30,7 @@ const Form = () => {
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea onClick={myRef.current++} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
             </form>
         </Container>
