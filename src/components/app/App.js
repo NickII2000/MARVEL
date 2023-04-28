3a8dfef8f45ee8deb5ad7ab55da4f9cb70c2813b
Nick <Nick_II2000@mail.ru>
2023-04-28 08:22:34 +0600

    450-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 const Form = () => {
     const [text, setText] = useState('');
+    const [textArea, setTextArea] = useState('');
 
     const validateInput = (text) => text.search(/\d/) >= 0;
 
@@ -13,7 +14,7 @@ const Form = () => {
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <input value={text} type="text" className="form-control" readOnly />
+                    <input value={`${text} / ${textArea}`} type="text" className="form-control" readOnly />
                     <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
                     <input
                         onChange={(e) => setText(e.target.value)}
@@ -25,7 +26,10 @@ const Form = () => {
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3"></textarea>
                 </div>
             </form>
         </Container>
