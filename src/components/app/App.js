a7abb491464a64509dd8c6c6c7aa5c355401eb36
Nick <Nick_II2000@mail.ru>
2023-04-28 07:37:33 +0600

    447-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,7 +10,7 @@ const Form = () => {
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
                     <input value={text} type="text" className="form-control" readOnly />
-                    <label htmlFor="exampleFormControlInput1" className="form-label mt-1">Email address</label>
+                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
                     <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                 </div>
                 <div className="mb-3">
