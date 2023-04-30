f94570a0c48cefdbc188ce5d59cbf243bd68bff9
Nick <Nick_II2000@mail.ru>
2023-04-30 19:18:26 +0600

    464-th commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import App from './components/app/App';
+import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <App />,
+  <App counter={0} />,
   document.getElementById('root')
 );
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './components/app/App';
-
-// import './style/style.scss';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
