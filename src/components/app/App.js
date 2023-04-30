0ca622130b46c7edecbeb2d8d1ce330dd6dd4ddf
Nick <Nick_II2000@mail.ru>
2023-04-30 18:20:32 +0600

    461-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -11,7 +11,7 @@ function useInputWithValidate(initialValue) {
 
     // const x = value.search(/\d/);
     // const validateInput = (value) => true;
-    const validateInput = (value) => value.search(/\d/) >= 0;
+    const validateInput = () => value.search(/\d/) >= 0;
 
     // return { value: value, onChange: onChange, validateInput: validateInput };
     return { value, onChange, validateInput };
