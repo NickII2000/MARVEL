166693ad7a81629cec8145aac330794e1694d3c2
Nick <Nick_II2000@mail.ru>
2023-05-05 21:02:57 +0600

    489-th commit

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const Counter = (props) => {
 }
 
 const RndCounter = (props) => {
-    const { counter, rndCounter, resetCounter } = useCounter(99990);
+    const { counter, rndCounter, resetCounter } = useCounter(999990);
 
     return (
         <div className="component">
