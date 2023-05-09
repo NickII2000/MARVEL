846b18f79679c03da93b22774ad26533e97e2171
Nick <Nick_II2000@mail.ru>
2023-05-09 20:16:35 +0600

    505-th commit

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function useCounter(initial) {
 }
 
 const Counter = (props) => {
-    const { counter, incCounter, decCounter, rndCounter, resetCounter } = useCounter(09);
+    const { counter, incCounter, decCounter, rndCounter, resetCounter } = useCounter(098);
 
     return (
         <div className="component">
