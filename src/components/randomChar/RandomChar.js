e022a3e1171df637b42076d38eb70a54a8acb923
Nick <Nick_II2000@mail.ru>
2023-05-10 08:31:04 +0600

    520-th commit

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
@@ -9,10 +9,9 @@ import mjolnir from '../../resources/img/mjolnir.png';
 const RandomChar = () => {
 
     const [char, setChar] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-
-    const marvelService = new MarvelService();
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(false);
+    const { loading, error, getCharacter } = useMarvelService();
 
     useEffect(() => {
         updateChar();
@@ -24,25 +23,24 @@ const RandomChar = () => {
     }, [])
 
     const onCharLoaded = (char) => {
-        setLoading(false);
+        // setLoading(false);
         setChar(char);
     }
 
-    const onCharLoading = () => {
-        setLoading(true);
-        setError(false);
-    }
+    // const onCharLoading = () => {
+    //     setLoading(true);
+    //     setError(false);
+    // }
 
-    const onError = () => {
-        setError(true);
-        setLoading(false);
-    }
+    // const onError = () => {
+    //     setError(true);
+    //     setLoading(false);
+    // }
 
     const updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        onCharLoading();
-        marvelService
-            .getCharacter(id)
+        // onCharLoading();
+        getCharacter(id)
             .then(onCharLoaded)
             .catch(onError);
     }
