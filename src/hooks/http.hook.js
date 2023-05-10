df924786b81e4a98a2ac2ac620a8807eb1df4438
Nick <Nick_II2000@mail.ru>
2023-05-10 07:21:44 +0600

    511-th commit

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -4,7 +4,16 @@ export const useHttp = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const request = useCallback(async () => {
+    const request = useCallback(async (url, method = 'GET', body = null, headers = { 'Content-Type': 'application/json' }) => {
+
+        setLoading(true);
+
+        try {
+            const response = await fetch(url, { method, body, headers });
+
+        } catch (e) {
+
+        }
 
     }, []);
 }
