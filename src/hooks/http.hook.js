ed8fbc2a81dfb283d2fe5463aaea5282fc41e4ea
Nick <Nick_II2000@mail.ru>
2023-05-10 07:11:07 +0600

    510-th commit

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,10 @@
+import { useState, useCallback } from "react";
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async () => {
+
+    }, []);
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/app/App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/style.scss';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
