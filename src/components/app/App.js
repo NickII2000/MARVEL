0bc44895ac7bd1fa8b8e6a6978c4de8b4416f451
Nick <Nick_II2000@mail.ru>
2023-05-16 20:52:52 +0600

    593-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -25,24 +25,27 @@ const App = () => {
             <div className="app">
                 <AppHeader />
                 <main>
-                    <Route path="/">
-                        <ErrorBoundary>
-                            <RandomChar />
-                        </ErrorBoundary>
-                        <div className="char__content">
+                    <Switch>
+                        <Route path="/comics">
+                            <AppBanner />
+                            <ComicsList />
+                        </Route>
+                        <Route path="/">
                             <ErrorBoundary>
-                                <CharList onCharSelected={onCharSelected} />
+                                <RandomChar />
                             </ErrorBoundary>
-                            <ErrorBoundary>
-                                <CharInfo charId={selectedChar} />
-                            </ErrorBoundary>
-                        </div>
-                        <img className="bg-decoration" src={decoration} alt="vision" />
-                    </Route>
-                    <Route path="/comics">
-                        <AppBanner />
-                        <ComicsList />
-                    </Route>
+                            <div className="char__content">
+                                <ErrorBoundary>
+                                    <CharList onCharSelected={onCharSelected} />
+                                </ErrorBoundary>
+                                <ErrorBoundary>
+                                    <CharInfo charId={selectedChar} />
+                                </ErrorBoundary>
+                            </div>
+                            <img className="bg-decoration" src={decoration} alt="vision" />
+                        </Route>
+
+                    </Switch>
                 </main>
             </div>
         </Router>
