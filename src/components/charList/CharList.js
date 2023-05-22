2f08a12f7bbc61488ddfe4fe29e14d1cb66150d0
Nick <Nick_II2000@mail.ru>
2023-05-22 08:24:33 +0600

    639-th commit

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -26,9 +26,11 @@ const CharList = (props) => {
             .then(onCharListLoaded);
     }
 
-    const onCharListLoaded = (newCharList) => {
+    const onCharListLoaded = async (newCharList) => {
 
         const { logger, secondLog } = await import('./someFunc');
+        logger();
+        secondLog();
 
         let ended = false;
         if (newCharList.length < 9) {
