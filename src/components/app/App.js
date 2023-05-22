425b79e9fa8d7233d1e5c23e98464fd4776054ac
Nick <Nick_II2000@mail.ru>
2023-05-22 08:55:56 +0600

    643-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,14 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { MainPage, ComicsPage, SingleComicPage } from '../pages';
+// import { MainPage, ComicsPage, SingleComicPage } from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from "../spinner/Spinner";
 
 const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
 
