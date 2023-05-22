35a1286df9abd047bd8e3a25eb40c40829f23fc9
Nick <Nick_II2000@mail.ru>
2023-05-22 08:33:46 +0600

    641-th commit

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -98,11 +98,11 @@ const CharList = (props) => {
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading && !newItemLoading ? <Spinner /> : null;
 
-    if (loading) {
-        import('./someFunc')
-            .then(obj => obj.default())
-            .catch();
-    }
+    // if (loading) {
+    //     import('./someFunc')
+    //         .then(obj => obj.default())
+    //         .catch();
+    // }
 
     return (
         <div className="char__list">
