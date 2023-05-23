d5f96e559d4b4082c2e8c952e4bb0157c6148c96
Nick <Nick_II2000@mail.ru>
2023-05-23 08:09:55 +0600

    644-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,42 +1,87 @@
-import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+// import './App.css';
 
-// import { MainPage, ComicsPage, SingleComicPage } from '../pages';
-import AppHeader from "../appHeader/AppHeader";
-import Spinner from "../spinner/Spinner";
-
-const Page404 = lazy(() => import('../pages/404'));
-const MainPage = lazy(() => import('../pages/MainPage'));
-const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
-
-const App = () => {
+const Form = (props) => {
 
     return (
-        <Router>
-            <div className="app">
-                <AppHeader />
-                <main>
-                    <Suspense fallback={<Spinner />}>
-                        <Switch>
-                            <Route exact path="/">
-                                <MainPage />
-                            </Route>
-                            <Route exact path="/comics">
-                                <ComicsPage />
-                            </Route>
-                            <Route exact path="/comics/:comicId">
-                                <SingleComicPage />
-                            </Route>
-                            <Route path='*'>
-                                <Page404 />
-                            </Route>
-                        </Switch>
-                    </Suspense>
-                </main>
-            </div>
-        </Router>
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                    <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
     )
 }
 
+function App() {
+    const [data, setData] = useState({
+        mail: "name@example.com",
+        text: 'some text'
+    });
+
+    return (
+        <>
+            <Form mail={data.mail} text={data.text} />
+            <br />
+            <button
+                onClick={() => setData({
+                    mail: "second@example.com",
+                    text: 'another text'
+                })}>
+                Click me
+            </button>
+        </>
+    );
+}
+
 export default App;
+
+// import { lazy, Suspense } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// // import { MainPage, ComicsPage, SingleComicPage } from '../pages';
+// import AppHeader from "../appHeader/AppHeader";
+// import Spinner from "../spinner/Spinner";
+
+// const Page404 = lazy(() => import('../pages/404'));
+// const MainPage = lazy(() => import('../pages/MainPage'));
+// const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+// const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+
+// const App = () => {
+
+//     return (
+//         <Router>
+//             <div className="app">
+//                 <AppHeader />
+//                 <main>
+//                     <Suspense fallback={<Spinner />}>
+//                         <Switch>
+//                             <Route exact path="/">
+//                                 <MainPage />
+//                             </Route>
+//                             <Route exact path="/comics">
+//                                 <ComicsPage />
+//                             </Route>
+//                             <Route exact path="/comics/:comicId">
+//                                 <SingleComicPage />
+//                             </Route>
+//                             <Route path='*'>
+//                                 <Page404 />
+//                             </Route>
+//                         </Switch>
+//                     </Suspense>
+//                 </main>
+//             </div>
+//         </Router>
+//     )
+// }
+
+// export default App;
