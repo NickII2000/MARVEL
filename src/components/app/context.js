0bf1638bf7b409ed32940023ef5629828e95f51a
Nick <Nick_II2000@mail.ru>
2023-05-27 20:04:25 +0600

    694-th commit

--- src/components/app/context.js
+++ src/components/app/context.js
@@ -1,5 +1,9 @@
 import { createContext } from 'react';
 
-const dataContext = createContext();
+const dataContext = createContext({
+    mail: "default@example.com",
+    text: 'default  text',
+    fourceChangeMail: () => { }
+});
 
 export default dataContext;
