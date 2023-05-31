67e69c6f1fa09f6732902c5bed493239d0d59957
Nick <Nick_II2000@mail.ru>
2023-05-31 08:05:31 +0600

    703-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,78 +1,40 @@
-import { useState } from 'react';
-// import './App.css';
-import Form from './Form';
-import dataContext from './context';
+import {useState} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
 
-const { Provider } = dataContext;
+const Slider = () => {
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
 
-function App() {
-    const [data, setData] = useState({
-        mail: "name@example.com",
-        text: 'some text',
-        forceChangeMail: forceChangeMail
-    });
-
-    function forceChangeMail() {
-        setData({ ...data, mail: 'test@gmail.com' });
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
     }
 
     return (
-        <Provider value={data}>
-            <Form text={data.text} />
-            <br />
-            <button
-                onClick={() => setData({
-                    ...data,
-                    mail: "second@example.com",
-                    text: 'another text',
-                })}>
-                Click me
-            </button>
-        </Provider>
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => setAutoplay(!autoplay)}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+function App() {
+    return (
+        <Slider/>
     );
 }
 
 export default App;
-
-// import { lazy, Suspense } from 'react';
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-// // import { MainPage, ComicsPage, SingleComicPage } from '../pages';
-// import AppHeader from "../appHeader/AppHeader";
-// import Spinner from "../spinner/Spinner";
-
-// const Page404 = lazy(() => import('../pages/404'));
-// const MainPage = lazy(() => import('../pages/MainPage'));
-// const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-// const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
-
-// const App = () => {
-
-//     return (
-//         <Router>
-//             <div className="app">
-//                 <AppHeader />
-//                 <main>
-//                     <Suspense fallback={<Spinner />}>
-//                         <Switch>
-//                             <Route exact path="/">
-//                                 <MainPage />
-//                             </Route>
-//                             <Route exact path="/comics">
-//                                 <ComicsPage />
-//                             </Route>
-//                             <Route exact path="/comics/:comicId">
-//                                 <SingleComicPage />
-//                             </Route>
-//                             <Route path='*'>
-//                                 <Page404 />
-//                             </Route>
-//                         </Switch>
-//                     </Suspense>
-//                 </main>
-//             </div>
-//         </Router>
-//     )
-// }
-
-// export default App;
