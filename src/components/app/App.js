02be197da566b45df5124b2ab5e9b455f5f5bcbe
Nick <Nick_II2000@mail.ru>
2023-06-04 19:12:01 +0600

    729-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -35,7 +35,6 @@ const Slider = ({ initial }) => {
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
                 <div className="text-center mt-5">Active slide {slide} <br />{"" + autoplay.autoplay}</div>
-                ""
                 <div className="buttons mt-3">
                     <button
                         className="btn btn-primary me-2"
