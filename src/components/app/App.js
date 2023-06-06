1df4115919d749023f6df2fabb0911b708741631
Nick <Nick_II2000@mail.ru>
2023-06-06 07:22:54 +0600

    735-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,30 +1,16 @@
-import { useState, useReducer } from 'react';
-import { Container } from 'react-bootstrap';
+import {useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'toggle':
-            return { autoplay: !state.autoplay };
-        case 'slow':
-            return { autoplay: 300 };
-        case 'fast':
-            return { autoplay: 700 };
-        case 'custom':
-            return { autoplay: action.payload };
-        default:
-            throw new Error();
-    }
-}
+const getDataFromFirstFetch = () => {return 10};
+const getDataFromSecondFetch = () => {return 20};
 
-function init(initial) {
-    return { autoplay: initial };
-}
-
-const Slider = ({ initial }) => {
+const SliderFirst = () => {
     const [slide, setSlide] = useState(0);
-    // const [autoplay, setAutoplay] = useState(false);
-    const [autoplay, dispatch] = useReducer(reducer, initial, init);
+
+    useEffect(() => {
+        setSlide(getDataFromFirstFetch());
+    }, [])
 
     function changeSlide(i) {
         setSlide(slide => slide + i);
@@ -34,35 +20,59 @@ const Slider = ({ initial }) => {
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br />{autoplay.autoplay !== false ? 'auto' : null} {autoplay.autoplay === 300 ? ' slow' : null} {autoplay.autoplay === 700 ? ' fast' : null} {autoplay.autoplay === 5000 ? ' custom' : null}</div>
+                <div className="text-center mt-5">Active slide {slide}</div>
                 <div className="buttons mt-3">
-                    <button
+                    <button 
                         className="btn btn-primary me-2"
                         onClick={() => changeSlide(-1)}>-1</button>
-                    <button
+                    <button 
                         className="btn btn-primary me-2"
                         onClick={() => changeSlide(1)}>+1</button>
-                    <button
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+const SliderSecond = () => {
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false)
+
+    useEffect(() => {
+        setSlide(getDataFromSecondFetch());
+    }, [])
+
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
+    }
+
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
                         className="btn btn-primary me-2"
-                        onClick={() => dispatch({ type: 'toggle' })} > toggle autoplay</button>
-                    <button
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button 
                         className="btn btn-primary me-2"
-                        onClick={() => dispatch({ type: 'slow' })} > slow autoplay</button>
-                    <button
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button 
                         className="btn btn-primary me-2"
-                        onClick={() => dispatch({ type: 'fast' })} > fast autoplay</button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={(e) => dispatch({ type: 'custom', payload: +e.target.textContent })} > 5000</button>
-                </div >
-            </div >
-        </Container >
+                        onClick={() => setAutoplay(autoplay => !autoplay)}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
     )
 }
 
 function App() {
     return (
-        <Slider initial={false} />
+        <>
+            <SliderFirst/>
+            <SliderSecond/>
+        </>
     );
 }
 
