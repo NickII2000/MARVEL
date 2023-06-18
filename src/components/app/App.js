1fbc8016d6435cd095002441342abba169571a9e
Nick <Nick_II2000@mail.ru>
2023-06-18 22:02:05 +0600

    771-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -135,8 +135,8 @@ function App() {
             <SliderSecond /> */}
             {/* <R1 /> */}
             <HelloWithLogger />
-            <SliderWithFirstFetch name={'name109'} />
-            <SliderWithSecondFetch name={'name20'} />
+            <SliderWithFirstFetch name={'name10'} />
+            <SliderWithSecondFetch name={'name2'} />
         </>
     );
 }
