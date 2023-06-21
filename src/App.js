62b96b65276c0bb1f96069c081fb96ca8f84862a
Nick <Nick_II2000@mail.ru>
2023-06-21 07:38:39 +0600

    779-th commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import {Container} from 'react-bootstrap';
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 const Modal = (props) => {
@@ -8,17 +8,17 @@ const Modal = (props) => {
         <div className="modal mt-5 d-block">
             <div className="modal-dialog">
                 <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Typical modal window</h5>
-                    <button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                    <p>Modal body content</p>
-                </div>
-                <div className="modal-footer">
-                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
-                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
-                </div>
+                    <div className="modal-header">
+                        <h5 className="modal-title">Typical modal window</h5>
+                        <button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <p>Modal body content</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
+                        <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -30,9 +30,9 @@ function App() {
 
     return (
         <Container>
-            {showModal ? <Modal onClose={setShowModal}/> : null}
-            <button 
-                type="button" 
+            {showModal ? <Modal onClose={setShowModal} /> : null}
+            <button
+                type="button"
                 className="btn btn-warning mt-5"
                 onClick={() => setShowModal(true)}>Open Modal</button>
         </Container>
