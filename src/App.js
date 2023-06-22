13f1ddb34ebc3b14f7b144ba8d71df0774012844
Nick <Nick_II2000@mail.ru>
2023-06-22 23:01:10 +0600

    791-th commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 const Modal = (props) => {
 
-    const duration = 1500;
+    const duration = 3000;
 
     const defaultStyle = {
         transition: `all ${duration}ms ease-in-out`,
