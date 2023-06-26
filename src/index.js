cd951878367a1c6f2b298eb17710758e3b8f47f1
Nick <Nick_II2000@mail.ru>
2023-06-26 07:27:42 +0600

    809-th commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/style.scss';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// было в версии 17
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// ниже версия 18
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    // <React.StrictMode>
+    <App />
+    // </React.StrictMode>
+  );
