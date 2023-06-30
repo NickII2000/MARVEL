023d25f08408ef8f03116b0f27b26d1a60cf6e33
Nick <Nick_II2000@mail.ru>
2023-06-30 09:12:06 +0600

    843-th commit

--- src/Form.js
+++ src/Form.js
@@ -11,7 +11,7 @@ const CustomForm = () => {
                 email: 'qwerty@react.r',
                 amount: 123,
                 currency: 'RUB',
-                text: '1234567890123',
+                text: '1234567890',
                 terms: false,
             }}
             validationSchema={Yup.object({
