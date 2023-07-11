5a8a216e2adb5adc8dc7ef70173223451da41cf7
Nick <Nick_II2000@mail.ru>
2023-07-11 21:12:35 +0600

    885-th commit

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Helmet } from "react-helmet";
 
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -19,13 +18,6 @@ const MainPage = () => {
 
     return (
         <>
-            <Helmet>
-                <meta
-                    name="description"
-                    content="Marvel information portal"
-                />
-                <title>Marvel information portal</title>
-            </Helmet>
             <ErrorBoundary>
                 <RandomChar />
             </ErrorBoundary>
