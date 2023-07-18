6db9628f4e67e243749d51042e8084cd92313d88
Nick <Nick_II2000@mail.ru>
2023-07-18 21:00:33 +0600

    913-th commit

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -48,7 +48,7 @@ const RandomChar = () => {
             {spinner}
             {content} 
             */}
-            {setContent(process, View, char)}
+            {setContent(process, char ? View : () => setProcess('error'), char)}
             <div className="randomchar__static">
                 <p className="randomchar__title">
                     Random character for today!<br />
