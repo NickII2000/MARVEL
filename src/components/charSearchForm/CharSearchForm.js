89344d9088377ebb0891c8331e9d29ca6a08ee4b
Nick <Nick_II2000@mail.ru>
2023-07-23 18:04:47 +0600

    939-th commit

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -40,7 +40,7 @@ const CharSearchForm = () => {
         <div className="char__search-form">
             <Formik
                 initialValues={{
-                    charName: '9'
+                    charName: '98'
                 }}
                 validationSchema={Yup.object({
                     charName: Yup.string().required('This field is required')
