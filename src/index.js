e51b671caf2a56a75b930ace9f4baa58f4f0354d
Nick <Nick_II2000@mail.ru>
2024-04-04 22:44:53 +0600

    1235-th commit

--- src/index.js
+++ src/index.js
@@ -114,6 +114,8 @@ const reducer = (state = 0, action) => {
             return state + 1;
         case 'DEC':
             return state - 1;
+        case 'RND':
+            return state * action.payload;
         default:
             return state;
     }
@@ -141,6 +143,10 @@ document.getElementById('inc').addEventListener('click', () => {
 });
 document.getElementById('dec').addEventListener('click', () => {
     store.dispatch({ type: 'DEC' });
+});
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    store.dispatch({ type: 'RND', payload: value });
 });
 
 ReactDOM
