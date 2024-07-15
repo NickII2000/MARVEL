a49cf074b7c6dd79cb00033fd44270d756f4caec
Nick <Nick_II2000@mail.ru>
2024-07-15 20:43:31 +0600

    1266-th commit

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+const Counter = () => {
+    return (
+        <div class="jumbotron">
+            <h1>0</h1>
+            </br>
+            <button className="btn btn-primary">DEC</button>
+            <button className="btn btn-primary">INC</button>
+            <button className="btn btn-primary">RND</button>
+        </div >
+    )
+};
+
+export default Counter;
