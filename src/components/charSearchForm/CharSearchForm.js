1af7cf2ac8da9006e26a8ea277bd9b1bfdf9b7bc
Nick <Nick_II2000@mail.ru>
2024-07-16 19:59:46 +0600

    1272-th commit

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -40,7 +40,7 @@ const CharSearchForm = () => {
         <div className="char__search-form">
             <Formik
                 initialValues={{
-                    charName: '_1270_1271_127 _127 _127 _127 _127 _127 _127 _127 _1269'
+                    charName: '_1270_1271_1272_127 _127 _127 _127 _127 _127 _127 _1269'
                 }}
                 validationSchema={Yup.object({
                     charName: Yup.string().required('This field is required')
--- src/index.js
+++ src/index.js
@@ -107,8 +107,9 @@ import { legacy_createStore as createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
 // import { inc, dec, rnd } from './actions';
 import * as actions from './actions';
+import { Provider } from 'react-redux';
 
-import Counter from './components/Counter';
+import App from './components/App';
 
 const store = createStore(reducer);
 
@@ -121,17 +122,17 @@ const update = () => {
         .createRoot(document.getElementById('root'))
         .render(
             <React.StrictMode>
-                <>
-                    <Counter
-                        counter={getState().value}
-                        inc={inc}
-                        dec={dec}
-                        rnd={() => {
-                            const value = Math.floor(Math.random() * 10 - 5);
-                            rnd(value);
-                        }}
-                    />
-                </>
+
+                {/* <Counter
+                    counter={getState().value}
+                    inc={inc}
+                    dec={dec}
+                    rnd={() => {
+                        const value = Math.floor(Math.random() * 10 - 5);
+                        rnd(value);
+                    }}
+                /> */}
+                <App />
             </React.StrictMode >
         );
 };
